a993a7bdfc943d7fef7f04ba6fcc2faef716cb2b
Barret Schloerke <bigbear@iastate.edu>
2009-05-28 16:00:31 +0000

    Added "put" command and Text command
    git-svn-id: svn://ggobi.org/ggobi/packages/ggpairs@6259 478a9d2e-6004-0410-ac81-b4d184805fe2

--- R/ggpairs.r
+++ R/ggpairs.r
@@ -1,24 +1,24 @@
-ggpairs <- function (data, mapping = aes(), colour = "black") 
-{
 
+
+ggpairs <- function (data, mapping = aes(), colour = "black", ...) 
+{
 	data <- as.data.frame(data)
+	numCol <- ncol(data)
 
 	grid.newpage()
-	numCol <- ncol(data)
 	
-	pushViewport(viewport(layout = grid.layout(numCol + 1, numCol + 1, widths = c(0.5,rep(1,numCol)), heights = c(rep(1,numCol),0.5) )))
+	.v1 <<- viewport(
+#		x = unit(0.5, "npc") + unit(1,"lines"), 
+#		y = unit(0.5, "npc") + unit(1,"lines"), 
+		width=unit(1, "npc") - unit(3,"lines"), 
+		height=unit(1, "npc") - unit(3, "lines")
+	)
 	
-	
-	
-	vplayout <- function(x, y) 
-		viewport(layout.pos.row = x, layout.pos.col = y) 
-
-
-
+	.v2 <<- viewport(layout = grid.layout(numCol, numCol, widths = rep(1,numCol), heights = rep(1,numCol) ))
 
     grid <- expand.grid(x = 1:ncol(data), y = 1:ncol(data))
-#    grid <- subset(grid, x != y)
-cat("\nGRID\n");print(grid)
+
+#cat("\nGRID\n");print(grid)
 #print(dataTypes)
 
 
@@ -28,55 +28,95 @@ cat("\nGRID\n");print(grid)
         data.frame(xvar = names(data)[ycol], yvar = names(data)[xcol])
     }))
 
-cat("\n\n\nALL\n");print(all)
+#cat("\n\n\nALL\n");print(all)
 
 	dataTypes <- plot_types(data)
-cat("\n\n\nDATA TYPES\n");print(dataTypes)
+#cat("\n\n\nDATA TYPES\n");print(dataTypes)
 
 	for(i in 1:nrow(dataTypes))
 	{
 		type <- dataTypes[i,"Type"]
-#		dataSelect <- get_select_data(data,grid[i,])
+
 		posX <- dataTypes[i,"posx"]
 		posY <- dataTypes[i,"posy"]
-		dataSelect <- data[,c(posX,posY)]
 
-		print(head(dataSelect))
-#		if(type == "scatterplot")
-			p <- qplot(dataSelect[,1], dataSelect[,2])
-		if(type == "box-hori")
-			p <- qplot(dataSelect[,2], dataSelect[,1], geom="boxplot") + coord_flip()
-		if(type == "box-vert")
-			p <- qplot(dataSelect[,1], dataSelect[,2], geom="boxplot")
-#		if(type == "mosaic")
-		
-		if(type == "stat_bin-num")
+		dataSelect <- data[,c(posX,posY)]
+		colnames(dataSelect) <- c("X","Y")
+
+#print(head(dataSelect))
+		if(type == "scatterplot")
 		{
-			colnames(dataSelect) <- c("x","y")
-			p <- ggplot(dataSelect, aes(x = x)) + 
+		#reg	p <- qplot(dataSelect[,1], dataSelect[,2]) + geom_point(colour = colour)
+
+		#contour
+			p <- qplot(dataSelect[,1], dataSelect[,2], geom = "density2d", colour = I("black"))
+		
+		#filled contour	p <- ggplot(data = dataSelect, aes(x = X, y = Y)) + stat_density2d(aes(fill = ..level..), geom="polygon")
+		}
+		else if(type == "box-hori")
+		{
+			p <- qplot(dataSelect[,2], dataSelect[,1], geom="boxplot") + coord_flip() +  
+				opts(
+					axis.text.y = theme_text(
+						angle = 90, 
+						vjust = 0, 
+						colour = "grey50")
+				)
+		}
+		else if(type == "box-vert")
+			p <- qplot(dataSelect[,1], dataSelect[,2], geom="boxplot")
+		else if(type == "mosaic")
+		{
+			p <- ggfluctuation(table(dataSelect[,2], dataSelect[,1])) +  
+				opts(
+					axis.text.y = theme_text(
+						angle = 90, 
+						vjust = 0, 
+						colour = "grey50")
+				)
+		}
+		else if(type == "stat_bin-num")
+		{
+			colnames(dataSelect) <- c("X","Y")
+			p <- ggplot(dataSelect, aes(x = X)) + 
 				scale_x_continuous() + 
 				scale_y_continuous() + 
 				stat_density(
 					aes(
-						x = x, 
+						x = X, 
 						y = ..scaled.. * diff(range(x)) + min(x)
 					),
 					position = "identity", 
 					colour = "grey20", 
 					geom = "line"
 				)+
-				ylim(range(dataSelect[,"x"]))
+				ylim(range(dataSelect[,"X"]))
 
 		}
-		
-		if(type == "stat_bin-cat")
+		else if(type == "stat_bin-cat")
 		{
-			colnames(dataSelect) <- c("x","y")
-			#p <- qplot(dataSelect[,1],geom="bar", position = "stack")
-			p <- ggplot(dataSelect, aes(x=x))+labs(x=NULL,y=NULL) +  geom_bar( aes(
-						y = ..count.. *diff(range(1:2)) #diff(range(x)) + min(x) 
-					)
+#			colnames(dataSelect) <- c("X","Y")
+#			#p <- qplot(dataSelect[,1],geom="bar", position = "stack")
+#			p <- ggplot(dataSelect, aes(x=X))+labs(x=NULL,y=NULL) +  geom_bar( aes(
+#						y = ..count.. *diff(range(1:2)) #diff(range(x)) + min(x) 
+#					)
+#				)
+			
+			dataTmp <- as.factor(dataSelect[,1])
+			#print(tmp)
+			count <- c()
+			for(z in 1:length(levels(dataTmp)))
+				count <- c(count,length(dataTmp[dataTmp==levels(dataTmp)[z]]))
+				
+			p <- qplot(levels(dataTmp), count, geom="bar", stat="identity")	+ coord_flip() +  
+				opts(
+					axis.text.y = theme_text(
+						angle = 90, 
+						vjust = 0, 
+						colour = "grey50")
 				)
+
+
 #			p <- ggplot(dataSelect, aes(x=x)) + 
 #				stat_bin()
 #					aes(
@@ -89,7 +129,7 @@ cat("\n\n\nDATA TYPES\n");print(dataTypes)
 		
 		}
 		
-			
+		
 		if(type == "box-hori")
 		{
 			if( posX != 1)
@@ -113,111 +153,135 @@ cat("\n\n\nDATA TYPES\n");print(dataTypes)
 			}	
 		}
 		
-		p <- p + labs(x = NULL) + labs(y = NULL)
-				
-		p <- p + opts(plot.margin = unit(rep(0,4), "lines"))
+		p <- p + 
+			labs(x = NULL, y = NULL) + 
+			opts(plot.margin = unit(rep(0,4), "lines"))
 
 		
-		
-		print(p, vp = vplayout(as.numeric(posY), as.numeric(posX)+1)) 
-		
+		putPlot(as.numeric(posY),as.numeric(posX),p)
 		
 	}
 
-	for(i in 1:numCol)
-	{
-		pushViewport(vplayout(as.numeric(i), 1))
-		grid.text(names(data)[i],1,0.5,rot=90,just=c("centre","bottom"))
-		popViewport()
-	}
 
-	for(i in 1:numCol)
-	{
-		pushViewport(vplayout(numCol+1, i+1))
-		grid.text(names(data)[i],0.5,1,just=c("centre","top"))
-		popViewport()
-	}
-
-
+	#		
+	## viewport for Left Names
+	pushViewport(viewport(width=unit(1, "npc") - unit(2,"lines"), height=unit(1, "npc") - unit(3, "lines")))
 	
+	pushViewport(viewport(layout = grid.layout(numCol, numCol, widths = rep(1,numCol), heights = rep(1,numCol) )))
+
+	# Left Side
+	for(i in 1:numCol)
+	{
+		grid.text(names(data)[i],0,0.5,rot=90,just=c("centre","centre"), vp = .vplayout(as.numeric(i),1))
+	}
 
 
-#
-#
-#
-#    grid <- expand.grid(x = 1:ncol(data), y = 1:ncol(data))
-#    grid <- subset(grid, x != y)
-#
-#	
-#
-#print(grid)
-#
-#	
-#	boxRows <- dataTypes[,"Type"] == "box-vert"
-#	
-#	grid <- as.matrix(subset(grid,!boxRows))
-#	grid <- subset(grid,!is.na(c(grid[,1])))
-#print(grid)
-##print(dataTypes)
-#
-#
-#    all <- do.call("rbind", lapply(1:nrow(grid), function(i) {
-#        xcol <- grid[i, "x"]
-#        ycol <- grid[i, "y"]
-#        data.frame(xvar = names(data)[ycol], yvar = names(data)[xcol], 
-#            x = data[, xcol], y = data[, ycol], data)
-#    }))
-##print(head(all))
-#
-#    all$xvar <- factor(all$xvar, levels = names(data))
-#    all$yvar <- factor(all$yvar, levels = names(data))
-#
-#    densities <- do.call("rbind", lapply(1:ncol(data), function(i) {
-#        data.frame(xvar = names(data)[i], yvar = names(data)[i], 
-#            x = as.numeric(data[, i]))
-#    }))
-#
-##print(head(all))
-##print(head(densities))
-#	allTrick <- all[,c("xvar","yvar","x","y")]
-#	allTrick[,"x"] <- 1
-#	allTrick[,"y"] <- 1
-#	b <<- allTrick
-#    mapping <- defaults(mapping, aes_string(x = "x", y = "y"))
-#
-#    class(mapping) <- "uneval"
-#
-#    p <- ggplot(allTrick, mapping) + scale_x_continuous() + scale_y_continuous()+
-#		facet_grid(xvar ~ yvar, scales = "free") + 
-##		stat_density(
-##			aes(
-##				x = x, 
-##				y = ..scaled.. * diff(range(x)) + min(x)
-##			), 
-##			data = densities,
-##			position = "identity", 
-##			colour = "grey20", 
-##			geom = "line"
-##		)+
-#
-#		geom_point(colour = colour, na.rm = TRUE) #+ 
-##		geom_histogram(
-##			aes(x = x#, 
-##			#y = ..scaled.. * diff(range(x)) + min(x)
-##			), 
-##			data = densities, 
-# #       	position = "identity", 
-##			#colour = "grey20"#, 
-##			#geom = "line"
-##			)
-#	
-#	if("box-vert" %in% dataTypes[,1])
-#	{
-#		data_tmp <- get_select_data(all,dataTypes[dataTypes[,1]=="box-vert",])
-#
-#		print(head(data_tmp))
-#		p <- p+ geom_boxplot(data = data_tmp)
-#	}
-#	
-#	p#+ geom_point(na.rm = TRUE)
+	popViewport()# layout
+	popViewport()# spacing
+
+	#		
+	## viewport for Bottom Names
+	pushViewport(viewport(width=unit(1, "npc") - unit(3,"lines"), height=unit(1, "npc") - unit(2, "lines")))
+
+	pushViewport(viewport(layout = grid.layout(numCol, numCol, widths = rep(1,numCol), heights = rep(1,numCol) )))
+
+
+	# Bottom Side
+	for(i in 1:numCol)
+	{
+		grid.text(names(data)[i],0.5,0,just=c("centre","centre"), vp = .vplayout(numCol, i))
+	}
+
+	popViewport() #layout
+	popViewport() #spacing
+	
+	return()
+}
+
+.vplayout <- function(x, y) 
+	viewport(layout.pos.row = x, layout.pos.col = y) 
+
+
+
+putPlot <- function(rowFromTop, columnFromLeft, p, axes = TRUE)
+{
+##	print(.vps$first)
+#print(str(.v1))
+#print(str(.v2))
+
+	pushViewport(.v1) # labels on outside
+	pushViewport(.v2) # layout of plots
+
+	grid.rect(
+		gp=gpar(fill="white",lty = "blank"),
+		vp = .vplayout(rowFromTop, columnFromLeft)
+	)
+	
+	if(!axes)
+	{
+		p <- p +
+			labs(x=NULL,y=NULL) +
+			opts(
+				axis.text.x = theme_blank(), 
+				axis.text.y = theme_blank(), 
+				plot.margin = unit(rep(0,4), "lines")
+			)
+	}
+	
+	
+	print(p, vp = .vplayout(rowFromTop, columnFromLeft))
+	
+	popViewport()
+	popViewport()
+	
+	#cat("\n\nDone")
+}
+
+
+ggplot_Text <- function(xP, yP, label, ...)
+{
+
+	p <- ggplot(data = 
+			data.frame(
+				x0 = xP,
+				y0 = yP,
+				x1 = c(0), 
+				x2 = c(1), 
+				y1 = c(0), 
+				y2 = c(1)
+			),
+			aes(
+				xmin=x1,
+				xmax = x2, 
+				ymin = y1, 
+				ymax = y2
+			)
+		)+
+		geom_rect(fill= I("white")) + 
+		labs(x = "X", y = "Y")
+	
+	p <- p + geom_text(aes(x = x0, y = y0), label = label, ...)
+	
+	p
+
+}
+
+
+ggplot_Cor <- function(xVar, yVar)
+{
+
+	ggplot_Text(
+		x=0.5,
+		y=.5,
+		paste(
+			"Corr:\n",
+			signif(
+				cor(xVar,yVar),
+				3
+			),
+			sep="",collapse=""
+		),
+		size=8,
+		colour = "Black")
+
 }
