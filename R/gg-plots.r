0e77cb81cbd544b5c8c4c53cdc623cd8b6a8b1e3
Barret Schloerke <schloerke@gmail.com>
2012-09-21 03:25:55 -0500

    fixed issue where not all levels where accounted for in the cor plot

--- R/gg-plots.r
+++ R/gg-plots.r
@@ -173,15 +173,17 @@ ggally_cor <- function(data, mapping, corAlignPercent = 0.6, corSize = 3, ...){
 
     # put in correct order
     lev <- levels(data[[colorCol]])
-    ord <- rep(-1, NROW(cord))
-    for(i in 1:NROW(cord)) {
+    ord <- rep(-1, nrow(cord))
+    for(i in 1:nrow(cord)) {
       for(j in seq_along(lev)){
         if(identical(as.character(cord[i, colorCol]), as.character(lev[j]))) {
           ord[i] <- j
         }
       }
     }
-    cord <- cord[ord, ]
+    # print(order(ord[ord >= 0]))
+    # print(lev)
+    cord <- cord[order(ord[ord >= 0]), ]
 
     cord$label <- str_c(cord[[colorCol]], ": ", cord$ggally_cor)
 
@@ -209,8 +211,8 @@ ggally_cor <- function(data, mapping, corAlignPercent = 0.6, corSize = 3, ...){
     #element_bw() +
     theme(legend.position = "none")
 
-    xPos <- rep(corAlignPercent, length(lev)) * diff(xrange) + min(xrange)
-    yPos <- seq(from = 0.9, to = 0.2, length.out = length(lev) + 1) * diff(yrange) + min(yrange)
+    xPos <- rep(corAlignPercent, nrow(cord)) * diff(xrange) + min(xrange)
+    yPos <- seq(from = 0.9, to = 0.2, length.out = nrow(cord) + 1) * diff(yrange) + min(yrange)
     yPos <- yPos[-1]
     # print(range(yVal))
     # print(yPos)
