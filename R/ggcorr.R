e5f0c6a9d87eff0cad1f3466c5e92205c6b3d5cb
Barret Schloerke <schloerke@gmail.com>
2013-04-14 18:53:42 -0400

    added ggcorr from Francois Briatte.  Added '...' to feed into geom_text.  (had to remove the default of size = 4)

--- R/ggcorr.R
+++ R/ggcorr.R
@@ -1,19 +1,26 @@
 #' ggcorr - Plot a correlation matrix with ggplot2
-#' 
+#'
 #' Function for making a correlation plot starting from a data matrix, using ggplot2. The function is directly inspired by Tian Zheng and Yu-Sung Su's arm:corrplot function.
-#' 
+#'
 #' @export
 #' @param data a data matrix. Should contain numerical (continuous) data.
 #' @param method a character string giving a method for computing covariances in the presence of missing values. This must be (an abbreviation of) one of the strings \code{"everything"}, \code{"all.obs"}, \code{"complete.obs"}, \code{"na.or.complete"}, or \code{"pairwise.complete.obs"}. Defaults to \code{"pairwise"}.
 #' @param palette a ColorBrewer palette to be used for correlation coefficients. Defaults to \code{"RdYlGn"}.
-#' @seealso \code{\link{cor}} and \code{\link{corrplot}} in the \link{arm} package
-#' @author François Briatte \email{f.briatte@@ed.ac.uk}
+#' @param ... other arguements supplied to geom_text for the diagonal labels.  Arguments pertaining to the title or other items can be achieved through ggplot2 methods.
+#' @seealso \code{\link{cor}} and \code{\link[arm]{corrplot}}
+#' @author Francois Briatte \email{f.briatte@@ed.ac.uk}
 #' @examples
 #' # Basketball statistics provided by Nathan Yau at Flowing Data.
 #' nba <- read.csv("http://datasets.flowingdata.com/ppg2008.csv")
 #' ggcorr(nba[-1])
-
-ggcorr <- function(data, method = "pairwise", palette = "RdYlGn") {
+#' ggcorr(
+#'   nba[-1],
+#'   size = 3,
+#'   hjust = 0.75,
+#'   angle = -45,
+#'   palette = "PuOr" # colorblind safe, photocopy-able
+#' ) + labs(title = "Points Per Game")
+ggcorr <- function(data, method = "pairwise", palette = "RdYlGn", ...) {
 
   M <- cor(data[1:ncol(data)], use = method)
   M <- M * lower.tri(M)
@@ -26,17 +33,20 @@ ggcorr <- function(data, method = "pairwise", palette = "RdYlGn") {
                  label = cut(s, breaks = s)[-1])
   M$row <- factor(M$row, levels = (unique(as.character(M$variable))))
   diag <- subset(M, row == variable)
-  
+
   po.nopanel <- list(theme(
-    panel.background = element_blank(), 
+    panel.background = element_blank(),
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank(),
-    axis.text.x = element_text(angle = -90)))
-  
-  ggplot(M, aes(row, variable)) + 
+    axis.text.x = element_text(angle = -90))
+  )
+
+  ggplot(M, aes(row, variable)) +
     scale_fill_brewer(palette = palette, name = "Correlation\ncoefficient") +
     geom_tile(aes(fill = value), colour = "white") +
-    geom_text(data = diag, aes(label = variable, hjust = 1), size = 4) +
-    scale_x_discrete(breaks = NULL) + scale_y_discrete(breaks = NULL) +
-    labs(x = "", y = "") + po.nopanel
+    geom_text(data = diag, aes(label = variable), ...) +
+    scale_x_discrete(breaks = NULL) +
+    scale_y_discrete(breaks = NULL) +
+    labs(x = "", y = "") +
+    po.nopanel
 }
