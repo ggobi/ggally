9e3418243ba2dd66ef9a9e58312e4351d190b4ce
Barret Schloerke <schloerke@gmail.com>
2013-07-30 14:55:07 -0400

    changed 'circle' to 'point'

--- R/ggcorr.r
+++ R/ggcorr.r
@@ -12,7 +12,7 @@ if(getRversion() >= "2.15.1") {
 #' @param method a character string giving a method for computing covariances in the presence of missing values. This must be (an abbreviation of) one of the strings \code{"everything"}, \code{"all.obs"}, \code{"complete.obs"}, \code{"na.or.complete"}, or \code{"pairwise.complete.obs"}. Defaults to \code{"pairwise"}.
 #' @param palette a ColorBrewer palette to be used for correlation coefficients. Defaults to \code{"RdYlGn"}.
 #' @param name a character string for the legend that shows quintiles of correlation coefficients.
-#' @param geom the geom object to use. Accepts either \code{tile} (the default) or \code{circle}, to plot proportionally scaled circles.
+#' @param geom the geom object to use. Accepts either \code{tile} (the default) or \code{point}, to plot proportionally scaled circles.
 #' @param max_size the maximum size for circles, as passed to \code{scale_size_area} for proportional scaling. Defaults to \code{6}.
 #' @param ... other arguments supplied to geom_text for the diagonal labels.  Arguments pertaining to the title or other items can be achieved through ggplot2 methods.
 #' @seealso \code{\link{cor}} and \code{\link[arm]{corrplot}}
@@ -25,7 +25,7 @@ if(getRversion() >= "2.15.1") {
 #' # Custom options.
 #' ggcorr(
 #'   nba[-1],
-#'   geom = "circle",
+#'   geom = "point",
 #'   max_size = 6,
 #'   size = 3,
 #'   hjust = 0.75,
@@ -33,7 +33,7 @@ if(getRversion() >= "2.15.1") {
 #'   palette = "PuOr" # colorblind safe, photocopy-able
 #' ) + labs(title = "Points Per Game")
 ggcorr <- function(data, method = "pairwise", palette = "RdYlGn", name = "Correlation\ncoefficient", geom = "tile", max_size = 6, ...) {
-  
+
   M <- cor(data[1:ncol(data)], use = method)
   M <- M * lower.tri(M)
   M <- as.data.frame(M)
@@ -46,17 +46,17 @@ ggcorr <- function(data, method = "pairwise", palette = "RdYlGn", name = "Correl
   M$row = factor(M$row, levels = (unique(as.character(M$variable))))
   M$num = as.numeric(M$value)
   diag <- subset(M, row == variable)
-  
+
   po.nopanel <- list(theme(
     panel.background = element_blank(),
     panel.grid.minor = element_blank(),
     panel.grid.major = element_blank(),
     axis.text.x = element_text(angle = -90))
   )
-  
+
   p = ggplot(M, aes(row, variable))
-  
-  if(geom == "circle") {
+
+  if(geom == "point") {
     p = p +
       scale_colour_brewer(name, palette = palette) +
       scale_size_area(name, max_size= max_size,
@@ -68,7 +68,7 @@ ggcorr <- function(data, method = "pairwise", palette = "RdYlGn", name = "Correl
       scale_fill_brewer(name, palette = palette) +
       geom_tile(aes(fill = value), colour = "white")
   }
-  
+
   p = p  +
     geom_text(data = diag, aes(label = variable), ...) +
     scale_x_discrete(breaks = NULL) +
@@ -76,6 +76,6 @@ ggcorr <- function(data, method = "pairwise", palette = "RdYlGn", name = "Correl
     labs(x = "", y = "") +
     coord_equal() +
     po.nopanel
-  
+
   return(p)
 }
