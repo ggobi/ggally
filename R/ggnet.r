8193a91813fe865abf136a899d3ec390463770e5
Barret Schloerke <schloerke@gmail.com>
2014-01-02 23:24:34 -0500

    shortened ggnet example time

--- R/ggnet.r
+++ R/ggnet.r
@@ -2,6 +2,7 @@ if(getRversion() >= "2.15.1") {
   utils::globalVariables(c("X1", "X2", "Y1", "Y2", "group", "id"))
 }
 
+
 #' ggnet - Plot a network with ggplot2
 #'
 #' Function for making a network plot from an object of class \code{network} or \code{igraph}, using ggplot2.  Please visit \url{http://github.com/briatte/ggnet} for the latest development and descriptions about ggnet.
@@ -44,7 +45,8 @@ if(getRversion() >= "2.15.1") {
 #' rnd
 #'
 #' # random network
-#' ggnet(rnd, label = TRUE, alpha = 1, color = "white", segment.color = "grey10")
+#' pRnd <- ggnet(rnd, label = TRUE, alpha = 1, color = "white", segment.color = "grey10")
+#' # pRnd
 #'
 #' # random groups
 #' category = LETTERS[rbinom(x, 4, .5)]
@@ -57,8 +59,9 @@ if(getRversion() >= "2.15.1") {
 #' # plot cities, firms and law firms
 #' type = network::get.vertex.attribute(cities, "type")
 #' type = ifelse(grepl("City|Law", type), gsub("I+", "", type), "Firm")
-#' ggnet(cities, mode = "kamadakawai", alpha = .2, node.group = type,
+#' pRnd <- ggnet(cities, mode = "kamadakawai", alpha = .2, node.group = type,
 #'       label = c("Paris", "Beijing", "Chicago"), color = "darkred")
+#' # pRnd
 
 
 ggnet <- function(
