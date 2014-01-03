f3382b4ea377b959017de4308b3f4f32bc87ecf0
Barret Schloerke <schloerke@gmail.com>
2014-01-02 23:22:29 -0500

    added id as global var in ggnet

--- R/ggnet.r
+++ R/ggnet.r
@@ -1,5 +1,5 @@
 if(getRversion() >= "2.15.1") {
-  utils::globalVariables(c("X1", "X2", "Y1", "Y2", "group"))
+  utils::globalVariables(c("X1", "X2", "Y1", "Y2", "group", "id"))
 }
 
 #' ggnet - Plot a network with ggplot2
