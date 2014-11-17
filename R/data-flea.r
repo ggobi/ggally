2a4ad762299733da4e0c6bd66d4d5e144bfe3e11
Dianne Cook <visnut@gmail.com>
2014-11-17 10:57:27 -0600

    added a glyph plot to the package, and two new data sets

---
+++ R/data-flea.r
@@ -0,0 +1,24 @@
+#' Historical data used for classification examples.
+#'
+#' This data contains physical measurements on three species of flea beetles. 
+#'
+#' @details \itemize{
+#'   \item species Ch. concinna, Ch. heptapotamica, Ch. heikertingeri
+#'   \item tars1 width of the first joint of the first tarsus in microns
+#'   \item tars2 width of the second joint of the first tarsus in microns
+#'   \item head the maximal width of the head between the external edges of the eyes in 0.01 mm
+#'   \item aede1 the maximal width of the aedeagus in the fore-part in microns
+#'   \item aede2 the front angle of the aedeagus (1 unit = 7.5 degrees)
+#'   \item aede3 the aedeagus width from the side in microns
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name flea
+#' @usage data(flea)
+#' @format A data frame with 74 rows and 7 variables
+#' @references
+#' Lubischew, A. A. (1962), On the Use of Discriminant Functions in 
+#' Taxonomy, Biometrics 18:455-477.
+NULL
+
---
+++ R/data-nasa.r
@@ -0,0 +1,26 @@
+#' Data from the Data Expo JSM 2006.
+#'
+#' This data was provided by NASA for the competition. 
+#'
+#' The data shows 6 years of monthly measurements of a 24x24 spatial grid 
+#' from Central America:
+#'
+#' @details \itemize{
+#'   \item time integer specifying temporal order of measurements
+#'   \item x, y, lat, long spatial location of measurements.
+#'   \item cloudhigh, cloudlow, cloudmid, ozone, pressure, surftemp, temperature 
+#'   are the various satellite measurements.
+#'   \item date, day, month, year specifying the time of measurements.
+#'   \item id unique ide for each spatial position.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name nasa
+#' @usage data(nasa)
+#' @format A data frame with 41472 rows and 17 variables
+#' @references
+#' Murrell, P. (2010) The 2006 Data Expo of the American Statistical Association.
+#'   Computational Statistics, 25:551-554.
+NULL
+
