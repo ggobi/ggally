698d0d68976c4d76f91d9dc1427abae4f7a4c2b8
Barret Schloerke <schloerke@gmail.com>
2014-11-30 16:55:24 -0500

    added a 'if' around optional package network in example

--- R/ggnet.r
+++ R/ggnet.r
@@ -36,7 +36,7 @@ if(getRversion() >= "2.15.1") {
 #' @details The \code{weight.method} argument produces visually scaled nodes that are proportionally sized to their unweighted degree. To compute weighted centrality or degree measures, see Tore Opsahl's \code{\link[tnet]{tnet}} package.
 #' @importFrom grid arrow
 #' @examples
-#' require(network)
+#' if(require(network)){
 #' # make toy random network
 #' x                  <- 10
 #' ndyads             <- x * (x - 1)
@@ -65,6 +65,7 @@ if(getRversion() >= "2.15.1") {
 #' pRnd <- ggnet(cityServiceFirms, mode = "kamadakawai", alpha = .2, node.group = type,
 #'       label.nodes = c("Paris", "Beijing", "Chicago"), color = "darkred")
 #' # pRnd
+#' }
 
 ggnet <- function(
   net,                          # an object of class network
