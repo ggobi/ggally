47733690ecc86ee64b88ca7a4cf27255c1468ba1
Barret Schloerke <schloerke@gmail.com>
2015-01-22 14:36:29 -0500

    more order tests for ggparcoord

--- tests/testthat/test-ggparcoord.R
+++ tests/testthat/test-ggparcoord.R
@@ -107,6 +107,22 @@ test_that("missing", {
   expect_true(TRUE)
 })
 
+test_that("order", {
+
+
+  for (ordering in c("Outlying","Skewed","Clumpy","Sparse","Striated","Convex","Skinny",
+    "Stringy","Monotonic")) {
+    p <- ggparcoord(data = diamonds.samp, columns = c(1,5:10), groupColumn = 2, order = ordering)
+    expect_true(all(levels(p$data) != c("carat", "depth", "table", "price", "x", "y", "z")))
+  }
+
+  for (ordering in c("skewness", "allClass", "anyClass")) {
+    p <- ggparcoord(data = diamonds.samp, columns = c(1,5:10), groupColumn = 2, order = ordering)
+    expect_true(all(levels(p$data) != c("carat", "depth", "table", "price", "x", "y", "z")))
+  }
+
+})
+
 test_that("basic", {
 
   # basic parallel coordinate plot, using default settings
