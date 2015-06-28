7889c05883adfd017a778b960068d73ecfb4ac0a
Barret Schloerke <schloerke@gmail.com>
2015-06-28 16:51:23 -0400

    added some scripts for reminders / needs work

---
+++ notes/color.R
@@ -0,0 +1,7 @@
+mtcars
+
+ggparcoord(mtcars, columns = c(1,3:6), mapping = aes(size = gear))
+
+
+dt <- melt(mtcars, c("mpg", "cyl", "disp", "hp", "drat", "wt", "qsec"))
+qplot(mpg, value, data = dt, color = variable, geom = "line")
