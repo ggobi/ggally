d40337ed21dcdbe8042f6d04c7d7c649b8e88f6d
François <briatte@gmail.com>
2015-09-10 12:43:31 +0200

    avoid sna/network/igraph conflicts in ggnet and ggnet2 tests

--- tests/testthat/test-ggnet2.R
+++ tests/testthat/test-ggnet2.R
@@ -1,6 +1,10 @@
 
 context("ggnet2")
 
+if ("package:igraph" %in% search()) {
+  detach("package:igraph")
+}
+
 require(network      , quietly = TRUE) # network objects
 require(sna          , quietly = TRUE) # placement and centrality
 
