3d77901c4d1fa376fa2c0f89016f83375d689851
Barret Schloerke <schloerke@gmail.com>
2015-09-20 00:31:11 -0400

    lintr: fixed trailing_whitespace_linter

--- R/ggpairs_add.R
+++ R/ggpairs_add.R
@@ -45,7 +45,7 @@
       # Get the name of what was passed in as e2, and pass along so that it
       # can be displayed in error messages
       # e2name <- deparse(substitute(e2))
-      
+
       if (is.null(e1$gg)) {
         e1$gg <- e2
       } else {
