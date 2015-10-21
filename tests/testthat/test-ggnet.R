d9f2bb62899038edf367dde98613f94e1a7ba023
François <briatte@gmail.com>
2015-10-21 12:12:30 +0200

    ggnet, ggnet2: slightly higher test coverage

--- tests/testthat/test-ggnet.R
+++ tests/testthat/test-ggnet.R
@@ -211,10 +211,13 @@ test_that("examples", {
   ### --- test igraph functionality
 
   # test igraph conversion
-  n = asIgraph(n)
-  p = ggnet(n)
+  p = ggnet(asIgraph(n))
   expect_null(p$guides$colour)
   expect_equal(length(p$layers), 2)
+
+  # test igraph degree
+  library(igraph)
+  ggnet(n, weight = "degree")
 
   expect_true(TRUE)
 
--- tests/testthat/test-ggnet2.R
+++ tests/testthat/test-ggnet2.R
@@ -230,9 +230,12 @@ test_that("examples", {
   ### --- test igraph functionality
 
   # test igraph conversion
-  n = asIgraph(n)
-  p = ggnet2(n, color = "group")
+  p = ggnet2(asIgraph(n), color = "group")
   expect_null(p$guides$colour)
+
+  # test igraph degree
+  library(igraph)
+  ggnet2(n, size = "degree")
 
   expect_true(TRUE)
 
