80a9058cd8c191c0de172d234abacd834dc389c2
Barret Schloerke <schloerke@gmail.com>
2016-06-05 13:29:27 -0400

    add tests to covr ggscatmat

--- tests/testthat/test-ggscatmat.R
+++ tests/testthat/test-ggscatmat.R
@@ -5,7 +5,10 @@ data(flea)
 
 test_that("example", {
 
-  expect_warning(p <- ggscatmat(flea, 1:3), "Factor variables are omitted in plot")
+  flea2 <- flea
+  flea2$species2 <- as.character(flea2$species)
+  expect_warning(p <- ggscatmat(flea2, c(1:3)), "Factor variables are omitted in plot")
+  expect_warning(p <- ggscatmat(flea2, c(2:3, 8)), "Factor variables are omitted in plot")
   expect_true(is.null(p$labels$colour))
   # print(p)
 
@@ -13,7 +16,13 @@ test_that("example", {
   expect_true(!is.null(p$labels$colour))
   # print(p)
 
+})
 
+test_that("corMethod", {
+  expect_silent({
+    p <- ggscatmat(flea, columns = 2:3, corMethod = "pearson")
+    p <- ggscatmat(flea, columns = 2:3, corMethod = "rsquare")
+  })
 })
 
 test_that("stops", {
