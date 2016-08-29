bb086c9ca052a09976f59ff800d5ddcdf358bcc5
Barret Schloerke <schloerke@gmail.com>
2016-08-29 12:30:29 -0400

    make examples more packagedocs friendly

--- R/ggnet.R
+++ R/ggnet.R
@@ -136,35 +136,33 @@ if (getRversion() >= "2.15.1") {
 #' @importFrom utils head installed.packages
 #' @importFrom grDevices gray.colors
 #' @examples
-#' if (require(network)){
+#' library(network)
 #'
-#'   # random adjacency matrix
-#'   x           <- 10
-#'   ndyads      <- x * (x - 1)
-#'   density     <- x / ndyads
-#'   m           <- matrix(0, nrow = x, ncol = x)
-#'   dimnames(m) <- list(letters[ 1:x ], letters[ 1:x ])
-#'   m[ row(m) != col(m) ] <- runif(ndyads) < density
-#'   m
+#' # random adjacency matrix
+#' x           <- 10
+#' ndyads      <- x * (x - 1)
+#' density     <- x / ndyads
+#' m           <- matrix(0, nrow = x, ncol = x)
+#' dimnames(m) <- list(letters[ 1:x ], letters[ 1:x ])
+#' m[ row(m) != col(m) ] <- runif(ndyads) < density
+#' m
 #'
-#'   # random undirected network
-#'   n <- network::network(m, directed = FALSE)
-#'   n
+#' # random undirected network
+#' n <- network::network(m, directed = FALSE)
+#' n
 #'
-#'   ggnet(n, label = TRUE, alpha = 1, color = "white", segment.color = "black")
+#' ggnet(n, label = TRUE, alpha = 1, color = "white", segment.color = "black")
 #'
-#'   # random groups
-#'   g <- sample(letters[ 1:3 ], 10, replace = TRUE)
+#' # random groups
+#' g <- sample(letters[ 1:3 ], 10, replace = TRUE)
 #'
-#'   # color palette
-#'   p <- c("a" = "steelblue", "b" = "forestgreen", "c" = "tomato")
+#' # color palette
+#' p <- c("a" = "steelblue", "b" = "forestgreen", "c" = "tomato")
 #'
-#'   ggnet(n, node.group = g, node.color = p, label = TRUE, color = "white")
+#' ggnet(n, node.group = g, node.color = p, label = TRUE, color = "white")
 #'
-#'   # edge arrows on a directed network
-#'   ggnet(network(m, directed = TRUE), arrow.gap = 0.05, arrow.size = 10)
-#'
-#' }
+#' # edge arrows on a directed network
+#' ggnet(network(m, directed = TRUE), arrow.gap = 0.05, arrow.size = 10)
 ggnet <- function(
   net,
   mode             = "fruchtermanreingold",
--- R/ggnet2.R
+++ R/ggnet2.R
@@ -199,59 +199,57 @@ if (getRversion() >= "2.15.1") {
 #' @importFrom utils installed.packages
 #' @importFrom grDevices gray.colors
 #' @examples
-#' if(require(network)) {
+#' library(network)
 #'
-#'   # random adjacency matrix
-#'   x           <- 10
-#'   ndyads      <- x * (x - 1)
-#'   density     <- x / ndyads
-#'   m           <- matrix(0, nrow = x, ncol = x)
-#'   dimnames(m) <- list(letters[ 1:x ], letters[ 1:x ])
-#'   m[ row(m) != col(m) ] <- runif(ndyads) < density
-#'   m
+#' # random adjacency matrix
+#' x           <- 10
+#' ndyads      <- x * (x - 1)
+#' density     <- x / ndyads
+#' m           <- matrix(0, nrow = x, ncol = x)
+#' dimnames(m) <- list(letters[ 1:x ], letters[ 1:x ])
+#' m[ row(m) != col(m) ] <- runif(ndyads) < density
+#' m
 #'
-#'   # random undirected network
-#'   n <- network::network(m, directed = FALSE)
-#'   n
+#' # random undirected network
+#' n <- network::network(m, directed = FALSE)
+#' n
 #'
-#'   ggnet2(n, label = TRUE)
-#'   ggnet2(n, label = TRUE, shape = 15)
-#'   ggnet2(n, label = TRUE, shape = 15, color = "black", label.color = "white")
+#' ggnet2(n, label = TRUE)
+#' ggnet2(n, label = TRUE, shape = 15)
+#' ggnet2(n, label = TRUE, shape = 15, color = "black", label.color = "white")
 #'
-#'   # add vertex attribute
-#'   x = network.vertex.names(n)
-#'   x = ifelse(x %in% c("a", "e", "i"), "vowel", "consonant")
-#'   n %v% "phono" = x
+#' # add vertex attribute
+#' x = network.vertex.names(n)
+#' x = ifelse(x %in% c("a", "e", "i"), "vowel", "consonant")
+#' n %v% "phono" = x
 #'
-#'   ggnet2(n, color = "phono")
-#'   ggnet2(n, color = "phono", palette = c("vowel" = "gold", "consonant" = "grey"))
-#'   ggnet2(n, shape = "phono", color = "phono")
+#' ggnet2(n, color = "phono")
+#' ggnet2(n, color = "phono", palette = c("vowel" = "gold", "consonant" = "grey"))
+#' ggnet2(n, shape = "phono", color = "phono")
 #'
-#'   if (require(RColorBrewer)) {
+#' if (require(RColorBrewer)) {
 #'
-#'     # random groups
-#'     n %v% "group" <- sample(LETTERS[1:3], 10, replace = TRUE)
+#'   # random groups
+#'   n %v% "group" <- sample(LETTERS[1:3], 10, replace = TRUE)
 #'
-#'     ggnet2(n, color = "group", palette = "Set2")
-#'
-#'   }
-#'
-#'   # random weights
-#'   n %e% "weight" <- sample(1:3, network.edgecount(n), replace = TRUE)
-#'   ggnet2(n, edge.size = "weight", edge.label = "weight")
-#'
-#'   # edge arrows on a directed network
-#'   ggnet2(network(m, directed = TRUE), arrow.gap = 0.05, arrow.size = 10)
-#'
-#'   # Padgett's Florentine wedding data
-#'   data(flo, package = "network")
-#'   flo
-#'
-#'   ggnet2(flo, label = TRUE)
-#'   ggnet2(flo, label = TRUE, label.trim = 4, vjust = -1, size = 3, color = 1)
-#'   ggnet2(flo, label = TRUE, size = 12, color = "white")
+#'   ggnet2(n, color = "group", palette = "Set2")
 #'
 #' }
+#'
+#' # random weights
+#' n %e% "weight" <- sample(1:3, network.edgecount(n), replace = TRUE)
+#' ggnet2(n, edge.size = "weight", edge.label = "weight")
+#'
+#' # edge arrows on a directed network
+#' ggnet2(network(m, directed = TRUE), arrow.gap = 0.05, arrow.size = 10)
+#'
+#' # Padgett's Florentine wedding data
+#' data(flo, package = "network")
+#' flo
+#'
+#' ggnet2(flo, label = TRUE)
+#' ggnet2(flo, label = TRUE, label.trim = 4, vjust = -1, size = 3, color = 1)
+#' ggnet2(flo, label = TRUE, size = 12, color = "white")
 ggnet2 <- function(
   net,
   mode             = "fruchtermanreingold",
