70181688685b32d75adb0846a8133e40c71fdd5c
Barret Schloerke <schloerke@gmail.com>
2016-09-12 15:13:05 -0400

    make ggcoef example library brrom for better output in rd

--- R/ggcoef.R
+++ R/ggcoef.R
@@ -22,15 +22,14 @@
 #' @param errorbar_size size of the error bars
 #' @param ... additional arguments sent to \code{\link[ggplot2]{geom_point}}
 #' @examples
-#' if (require(broom)) {
-#'   reg <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = iris)
-#'   ggcoef(reg)
+#' library(broom)
+#' reg <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = iris)
+#' ggcoef(reg)
 #'
-#'   d <- as.data.frame(Titanic)
-#'   reg2 <- glm(Survived ~ Sex + Age + Class, family = binomial, data = d, weights = d$Freq)
-#'   ggcoef(reg2, exponentiate = TRUE)
-#'   ggcoef(reg2, exponentiate = TRUE, exclude_intercept = TRUE, errorbar_height = .2, color = "blue")
-#' }
+#' d <- as.data.frame(Titanic)
+#' reg2 <- glm(Survived ~ Sex + Age + Class, family = binomial, data = d, weights = d$Freq)
+#' ggcoef(reg2, exponentiate = TRUE)
+#' ggcoef(reg2, exponentiate = TRUE, exclude_intercept = TRUE, errorbar_height = .2, color = "blue")
 #' @export
 ggcoef <- function(
   x,
