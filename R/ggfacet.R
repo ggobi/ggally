e681be119d417121fa0002aec1a71da6d156f4ef
Barret Schloerke <schloerke@gmail.com>
2016-09-19 13:23:36 -0400

    add a ggfacet fn

---
+++ R/ggfacet.R
@@ -0,0 +1,118 @@
+#' ggfacet - single ggplot2 plot matrix with facet_grid
+#'
+#'
+#' @param data data.frame that contains all columns to be displayed.  This data will be melted before being passed into the function \code{fn}
+#' @param mapping aesthetic mapping (besides \code{x} and \code{y}).  See \code{\link[ggplot2]{aes}()}
+#' @param fn function to be executed. Similar to \code{\link{ggpairs}} and \code{\link{ggduo}}, the function may either be a string identifier or a real function that \code{\link{wrap}} understands.
+#' @param ... extra arguments passed directly to \code{fn}
+#' @param columnsX columns to be displayed in the plot matrix
+#' @param columnsY rows to be displayed in the plot matrix
+#' @param columnLabelsX,columnLabelsY column and row labels to display in the plot matrix
+#' @param xlab,ylab,title plot matrix labels
+#' @param scales parameter supplied to \code{ggplot2::\link[ggplot2]{facet_grid}}. Default behavior is \code{"free"}
+#' @export
+#' @examples
+#' # Small function to display plots only if it's interactive
+#' p_ <- GGally::print_if_interactive
+#' data(mtcars)
+#'
+#' small_mtcars <- mtcars[c(1,3:6)]
+#'
+#' # Basic plot of all column pair combinations
+#' p_(ggfacet(small_mtcars))
+#'
+#' # Change scales to 'fixed' to display each plot on the same scale
+#' p_(ggfacet(small_mtcars, scales = "fixed"))
+#'
+#' # Change to ggally_smooth as plotting function
+#' p_(ggfacet(small_mtcars, fn = 'smooth'))
+#' # Same output
+#' p_(ggfacet(small_mtcars, fn = ggally_smooth))
+#'
+#' # Change ggally_smooth's smoothing method to 'loess'
+#' p_(ggfacet(small_mtcars, fn = "smooth", method = "loess"))
+#'
+#' # Look at different response variables in mtcars
+#' p_(ggfacet(mtcars, columnsX = 3:6, columnsY = c("mpg", "qsec"), fn = "smooth", method = "loess"))
+ggfacet <- function(
+  data, mapping = NULL,
+  columnsX = 1:ncol(data),
+  columnsY = 1:ncol(data),
+  fn = 'points', ...,
+  columnLabelsX = names(data[columnsX]),
+  columnLabelsY = names(data[columnsY]),
+  xlab = NULL,
+  ylab = NULL,
+  title = NULL,
+  scales = "free"
+) {
+
+  data <- fix_data(data)
+  fn <- wrap(fn)
+
+  # fix args
+  if (
+    !missing(mapping) & !is.list(mapping) &
+    !missing(columnsX) & missing(columnsY)
+  ) {
+    columnsY <- columnsX
+    columnsX <- mapping
+    mapping <- NULL
+  }
+
+  stop_if_bad_mapping(mapping)
+
+  columnsX <- fix_column_values(data, columnsX, columnLabelsX, "columnsX", "columnLabelsX")
+  columnsY <- fix_column_values(data, columnsY, columnLabelsY, "columnsY", "columnLabelsY")
+
+  is_factor_x <- sapply(data[columnsX], is.factor)
+  if (sum(is_factor_x) != 0) {
+    warning(paste(sum(is_factor_x), " factor variables are being removed from X columns", sep = ""))
+    columnsX <- columnsX[!is_factor_x]
+    columnLabelsX <- columnLabelsX[!is_factor_x]
+  }
+  is_factor_y <- sapply(data[columnsY], is.factor)
+  if (sum(is_factor_y) != 0) {
+    warning(paste(sum(is_factor_y), " factor variables are being removed from Y columns", sep = ""))
+    columnsY <- columnsY[!is_factor_y]
+    columnLabelsY <- columnLabelsY[!is_factor_y]
+  }
+
+  tall_data <- ddply(
+    expand.grid(.x_col = columnsX, .y_col = columnsY),
+    c(".x_col", ".y_col"),
+    function(row) {
+      x_var <- row$.x_col[1]
+      y_var <- row$.y_col[1]
+
+      ret <- data
+      ret[[".x_val"]] <- data[[x_var]]
+      ret[[".y_val"]] <- data[[y_var]]
+      ret
+    }
+  )
+
+  if (is.null(mapping)) {
+    mapping <- aes()
+  }
+  mapping[c("x", "y")] <- aes_string(x = ".x_val", y = ".y_val")
+
+  names(columnLabelsX) <- as.character(columnsX)
+  names(columnLabelsY) <- as.character(columnsY)
+  labeller <- function(vals) {
+    val_names <- names(vals)
+    if (".x_col" %in% val_names) {
+      vals[[".x_col"]] <- columnLabelsX[as.character(vals[[".x_col"]])]
+    }
+    if (".y_col" %in% val_names) {
+      # browser()
+      vals[[".y_col"]] <- columnLabelsY[as.character(vals[[".y_col"]])]
+    }
+    vals
+  }
+  p <- fn(tall_data, mapping, ...) +
+    facet_grid(.y_col ~ .x_col, labeller = labeller, scales = scales) +
+    labs(title = title, x = xlab, y = ylab)
+
+  p
+}
