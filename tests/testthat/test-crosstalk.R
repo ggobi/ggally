72bccade25d5e8295d15ab906770a412efb96f93
Barret Schloerke <schloerke@gmail.com>
2016-11-07 13:30:16 -0500

    add crosstalk test

---
+++ tests/testthat/test-crosstalk.R
@@ -0,0 +1,54 @@
+
+
+
+if (requireNamespace("crosstalk", quietly = TRUE)) {
+  context("crosstalk")
+
+  test_that("crosstalk works with ggduo and ggpairs", {
+
+
+    sd <- crosstalk::SharedData$new(iris[1:4])
+
+    expect_silent({
+      pm <- ggpairs(sd)
+    })
+    expect_error({
+        pm <- ggpairs(sd, 3:5)
+      },
+      "Make sure your numeric"
+    )
+    expect_error({
+        pm <- ggpairs(sd, c("Petal.Length", "Petal.Width", crosstalk_key()))
+      },
+      "Columns in 'columns' not"
+    )
+
+    expect_silent({
+      pm <- ggduo(sd)
+    })
+    expect_error({
+        pm <- ggduo(sd, c(1:2, 5), 3:5)
+      },
+      "Make sure your numeric 'columnsX'"
+    )
+    expect_error({
+        pm <- ggduo(
+          sd,
+          c("Sepal.Length", "Sepal.Width", crosstalk_key()),
+          c("Petal.Length", "Petal.Width")
+        )
+      },
+      "Columns in 'columnsX' not"
+    )
+    expect_error({
+        pm <- ggduo(
+          sd,
+          c("Sepal.Length", "Sepal.Width"),
+          c("Petal.Length", "Petal.Width", crosstalk_key())
+        )
+      },
+      "Columns in 'columnsY' not"
+    )
+
+  })
+}
