ea1a4f08f97e3a96d8579c1198f63c5d35932301
Barret Schloerke <schloerke@gmail.com>
2016-11-11 14:47:20 -0500

    added example for fn_switch

--- R/ggnostic.R
+++ R/ggnostic.R
@@ -442,6 +442,28 @@ ggally_nostic_hat <- function(
 #'
 #' @param types list of functions that follow the ggmatrix function standard: \code{function(data, mapping, ...){ #make ggplot2 object }}.  One key should be a 'default' key for a default switch case.
 #' @param mapping_val mapping value to switch on.  Defautls to the 'y' varaible of the aesthetics list.
+#' @examples
+#' ggnostic_continuous_fn <- fn_switch(list(
+#'   default = ggally_points,
+#'   .fitted = ggally_points,
+#'   .se.fit = ggally_nostic_se_fit,
+#'   .resid = ggally_nostic_resid,
+#'   .hat = ggally_nostic_hat,
+#'   .sigma = ggally_nostic_sigma,
+#'   .cooksd = ggally_nostic_cooksd,
+#'   .std.resid = ggally_nostic_std_resid
+#' ))
+#'
+#' ggnostic_combo_fn <- fn_switch(list(
+#'   default = ggally_box_no_facet,
+#'   fitted = ggally_box_no_facet,
+#'   .se.fit = ggally_nostic_se_fit,
+#'   .resid = ggally_nostic_resid,
+#'   .hat = ggally_nostic_hat,
+#'   .sigma = ggally_nostic_sigma,
+#'   .cooksd = ggally_nostic_cooksd,
+#'   .std.resid = ggally_nostic_std_resid
+#' ))
 fn_switch <- function(
   types,
   mapping_val = "y"
