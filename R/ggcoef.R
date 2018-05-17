6ff005fd8cb880156be32a5ea9a840f37a280717
Barret Schloerke <schloerke@gmail.com>
2018-05-17 02:08:19 -0400

    fix broken checks

--- R/ggcoef.R
+++ R/ggcoef.R
@@ -26,15 +26,13 @@
 #' library(broom)
 #' reg <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = iris)
 #' ggcoef(reg)
-#' if (requireNamespace("MASS", quietly = TRUE)) {
-#'   d <- as.data.frame(Titanic)
-#'   reg2 <- glm(Survived ~ Sex + Age + Class, family = binomial, data = d, weights = d$Freq)
-#'   ggcoef(reg2, exponentiate = TRUE)
-#'   ggcoef(
-#'     reg2, exponentiate = TRUE, exclude_intercept = TRUE,
-#'     errorbar_height = .2, color = "blue", sort = "ascending"
-#'   )
-#' }
+#' \donttest{d <- as.data.frame(Titanic)
+#' reg2 <- glm(Survived ~ Sex + Age + Class, family = binomial, data = d, weights = d$Freq)
+#' ggcoef(reg2, exponentiate = TRUE)
+#' ggcoef(
+#'   reg2, exponentiate = TRUE, exclude_intercept = TRUE,
+#'   errorbar_height = .2, color = "blue", sort = "ascending"
+#' )}
 #' @export
 ggcoef <- function(
   x,
--- tests/testthat/test-ggcoef.R
+++ tests/testthat/test-ggcoef.R
@@ -10,16 +10,15 @@ test_that("example", {
   reg <- lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width, data = iris)
   expect_print(ggcoef(reg))
 
+  skip_if_not_installed("MASS")
   d <- as.data.frame(Titanic)
   reg2 <- glm(Survived ~ Sex + Age + Class, family = binomial, data = d, weights = d$Freq)
-  if (requireNamespace("MASS", quietly = TRUE)) {
-    expect_print(ggcoef(reg2, exponentiate = TRUE))
-    expect_print(ggcoef(
-      reg2,
-      exponentiate = TRUE,
-      exclude_intercept = TRUE,
-      errorbar_height = .2,
-      color = "blue"
-    ))
-  }
+  expect_print(ggcoef(reg2, exponentiate = TRUE))
+  expect_print(ggcoef(
+    reg2,
+    exponentiate = TRUE,
+    exclude_intercept = TRUE,
+    errorbar_height = .2,
+    color = "blue"
+  ))
 })
