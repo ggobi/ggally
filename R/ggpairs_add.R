634e8b9c3c186fbf42cb76da23c75677b517b801
Joseph Larmarange <joseph@larmarange.net>
2020-04-24 12:13:59 +0200

    functions to add any ggproto object to a ggmatrix
    fix #319

--- R/ggpairs_add.R
+++ R/ggpairs_add.R
@@ -13,15 +13,15 @@
 ######   \item \code{uneval}: replace current aesthetics
 ######   \item \code{layer}: add new layer
 #'   \item \code{theme}: update plot theme
-######   \item \code{scale}: replace current scale
-######   \item \code{coord}: override current coordinate system
+#'   \item \code{scale}: replace current scale
+#'   \item \code{coord}: override current coordinate system
 ######   \item \code{facet}: override current coordinate faceting
 #' }
 #'
 #' The \code{+} operator completely replaces elements
 #' with elements from e2.
 #'
-#' @param e1 An object of class \code{ggplot} or \code{theme}
+#' @param e1 An object of class \code{ggmatrix}
 #' @param e2 A component to add to \code{e1}
 #'
 #' @export
@@ -55,9 +55,11 @@
     add_theme_to_ggmatrix(e1, e2)
   } else if (is.list(e2)) {
     add_list_to_ggmatrix(e1, e2)
+  } else if (is.ggproto(e2)) {
+    add_ggproto_to_ggmatrix(e1, e2)
   } else {
     stop(
-      "'ggmatrix' does not know how to add objects that do not have class 'theme' or 'labels'.",
+      "'ggmatrix' does not know how to add objects that do not have class 'theme', 'labels' or 'ggproto'.",
       " Received object with class: '", paste(class(e2), collapse = ", "), "'"
     )
   }
@@ -113,6 +115,47 @@ add_theme_to_ggmatrix <- function(e1, e2) {
     # calls ggplot2 add method and stores the result in gg
     e1$gg$theme <- e1$gg$theme %+% e2
   }
+  e1
+}
+
+#' @export
+#' @rdname gg-add
+#' @param rows Integer vector. Rows to modify
+#' @param cols Integer vector. Cols to modify
+#' @param only_fn Character vector. If specified, only subplots
+#'   of that type will be modified (see examples).
+#' @details
+#' \code{add_ggproto_to_ggmatrix} gives you more control to modify
+#'   only some subplots.
+#' @examples
+#' ## modify scale
+#' pm + scale_fill_brewer()
+#' ## only first row
+#' add_ggproto_to_ggmatrix(pm, scale_fill_brewer(), rows = 1)
+#' ## only certain types of subplots
+#' add_ggproto_to_ggmatrix(
+#'   pm, scale_fill_brewer(),
+#'   only_fn = c("ggally_box_no_facet", "ggally_barDiag")
+#' )
+add_ggproto_to_ggmatrix <- function(
+  e1, e2,
+  rows = 1:e1$nrow, cols = 1:e1$ncol,
+  only_fn = NULL) {
+  if (!is.ggmatrix(e1))
+    stop("e1 should be a ggmatrix.")
+  if (!is.ggproto(e2))
+    stop("e2 should be a ggproto object.")
+
+  for (i in rows) {
+    for (j in cols) {
+      pos <- get_pos(e1, i, j)
+      fn_name <- attr(e1$plots[[pos]]$fn, "name")
+      if (is.null(only_fn) | fn_name %in% only_fn) {
+        e1[i, j] <- e1[i, j] + e2
+      }
+    }
+  }
+
   e1
 }
 
