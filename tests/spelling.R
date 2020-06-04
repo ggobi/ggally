415e109e64cea0c031e7806c082b53303bbc06f4
Barret Schloerke <schloerke@gmail.com>
2020-06-04 01:39:46 -0400

    fix typo

--- tests/spelling.R
+++ tests/spelling.R
@@ -2,7 +2,7 @@
 
 # only check spelling if on CI and spelling is available
 if (requireNamespace('spelling', quietly = TRUE)) {
-  if (isTRUE(as.logical(Sys.getenv("CI"))) {
+  if (isTRUE(as.logical(Sys.getenv("CI")))) {
     spelling::spell_check_test(vignettes = TRUE, error = FALSE,
                               skip_on_cran = TRUE)
   }
