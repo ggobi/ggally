3b451bc0d898d61656bab73b3dd06d9cc16f77d6
Barret Schloerke <schloerke@gmail.com>
2020-06-04 01:25:21 -0400

    only spell check on CI. Safeguard testthat

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,9 @@
-if(requireNamespace('spelling', quietly = TRUE))
-  spelling::spell_check_test(vignettes = TRUE, error = FALSE,
-                             skip_on_cran = TRUE)
+# `devtools::spell_check()`
+
+# only check spelling if on CI and spelling is available
+if (requireNamespace('spelling', quietly = TRUE)) {
+  if (isTRUE(as.logical(Sys.getenv("CI"))) {
+    spelling::spell_check_test(vignettes = TRUE, error = FALSE,
+                              skip_on_cran = TRUE)
+  }
+}
