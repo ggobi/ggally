7dbc929a62b999fe4cb9f2777cf6020c10550ba1
Joseph Larmarange <joseph@larmarange.net>
2020-06-12 12:30:43 +0200

    better default values for ggbivariate

--- R/ggbivariate.R
+++ R/ggbivariate.R
@@ -76,6 +76,8 @@ ggbivariate <- function(
   }
 
   # default behavior
+  if (is.null(rowbar_args$remove_percentage_axis))
+    rowbar_args$remove_percentage_axis <- TRUE
   if (is.null(rowbar_args$remove_background))
     rowbar_args$remove_background <- TRUE
   if (is.null(types$discrete))
