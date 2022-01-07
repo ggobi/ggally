829586588f384c76f6a6d1af9c1d21fc60fab8f5
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-07 23:18:00 +1100

    use cubble::geom_glyph as backend for glyph related functions

--- R/gglyph.R
+++ R/gglyph.R
@@ -1,6 +1,9 @@
-#' Create \code{\link{glyphplot}} data
+#' Create data for glyph map
 #'
 #' Create the data needed to generate a glyph plot.
+#'
+#' The \code{glyphs} functions
+#' now extract the data object created with \code{\link{cubble::geom_glyph}}
 #'
 #' @param data A data frame containing variables named in \code{x_major},
 #'   \code{x_minor}, \code{y_major} and \code{y_minor}.
@@ -17,7 +20,8 @@
 #'  minor values within a grid cell.  Defaults to \code{\link{identity}} so
 #'  that no scaling is performed.
 #' @export
-#' @author Di Cook, Heike Hofmann, Hadley Wickham
+#' @importFrom cubble geom_glyph
+#' @author Di Cook, Heike Hofmann, Hadley Wickham, Sherry Zhang
 #' @examples
 #' # Small function to display plots only if it's interactive
 #' p_ <- GGally::print_if_interactive
@@ -46,88 +50,71 @@ glyphs <- function(
   y_scale = identity,
   x_scale = identity
 ) {
-  data$gid <- interaction(data[[x_major]], data[[y_major]], drop = TRUE)
+  out <- glyph_layer(data, component = "glyph",
+                     x_major, x_minor,
+                     y_major, y_minor,
+                    polar, height, width, y_scale, x_scale)
+  new_cols <- out %>%
+    dplyr::select(group, x, y) %>%
+    dplyr::rename(gid = group, gx = x, gy = y)
 
-  if (is.rel(width)) {
-    width <- resolution(data[[x_major]], zero = FALSE) * unclass(width)
-    message("Using width ", format(width, digits = 3))
-  }
+  data <- data %>% dplyr::bind_cols(new_cols)
 
-  if (is.rel(height)) {
-    height <- resolution(data[[y_major]], zero = FALSE) * unclass(height)
-    message("Using height ", format(height, digits = 3))
-  }
-
-  if (!identical(x_scale, identity) || !identical(y_scale, identity)) {
-    data <- ddply(data, "gid", function(df) {
-      df[[x_minor]] <- x_scale(df[[x_minor]])
-      df[[y_minor]] <- y_scale(df[[y_minor]])
-      df
-    })
-  }
-
-  if (polar) {
-    theta <- 2 * pi * rescale01(data[[x_minor]])
-    r <- rescale01(data[[y_minor]])
-
-    data$gx <- data[[x_major]] + width  / 2 * r * sin(theta)
-    data$gy <- data[[y_major]] + height / 2 * r * cos(theta)
-    data <- data[order(data[[x_major]], data[[x_minor]]), ]
-  } else {
-    data$gx <- data[[x_major]] + rescale11(data[[x_minor]]) * width / 2
-    data$gy <- data[[y_major]] + rescale11(data[[y_minor]]) * height / 2
-  }
-
-  structure(data,
-    width = width, height = height, polar = polar,
-    x_major = x_major, y_major = y_major,
-    class = c("glyphplot", "data.frame"))
+  tibble::new_tibble(data, nrow = nrow(data),
+            width = width, height = height, polar = polar,
+            x_major = x_major, y_major = y_major,
+            x_minor = x_minor, y_minor = y_minor,
+            class = "glyphplot")
 }
 
-# Create reference lines for a glyph plot
-ref_lines <- function(data) {
-  stopifnot(is.glyphplot(data))
 
-  glyph <- attributes(data)
 
-  cells <- unique(data[c(glyph$x_major, glyph$y_major, "gid")])
+glyph_layer <- function(data, component = c("glyph", "line", "box"),
+                        x_major, x_minor, y_major, y_minor,
+                       polar, height, width,
+                       y_scale, x_scale){
+  if (missing(x_major)) x_major <- attr(data, "x_major")
+  if (missing(x_minor)) x_minor <- attr(data, "x_minor")
+  if (missing(y_major)) y_major <- attr(data, "y_major")
+  if (missing(y_minor)) y_minor <- attr(data, "y_minor")
+  if (missing(polar)) polar <- attr(data, "polar")
+  if (missing(width)) width <- attr(data, "width")
+  if (missing(height)) height <- attr(data, "height")
 
-  if (glyph$polar) {
-    ref_line <- function(df) {
-      theta <- seq(0, 2 * pi, length.out = 30)
-      data.frame(
-        gid = df$gid,
-        gx = df[[glyph$x_major]] + glyph$width / 4 * sin(theta),
-        gy = df[[glyph$y_major]] + glyph$height / 4 * cos(theta)
-      )
+  p <- ggplot2::ggplot(data = data,
+                         aes(x_major = data[[x_major]], x_minor = data[[x_minor]],
+                             y_major = data[[y_major]], y_minor = data[[y_minor]]),
+                         polar = polar, height = height, width = width)
 
-    }
-  } else {
-    ref_line <- function(df) {
-      data.frame(
-        gid = df$gid,
-        gx = df[[glyph$x_major]] + c(-1, 1) * glyph$width / 2,
-        gy = df[[glyph$y_major]]
-      )
-    }
-  }
-  ddply(cells, "gid", ref_line)
+  out <- switch (component,
+    "glyph" = p + cubble::geom_glyph(),
+    "line" = p + cubble::geom_glyph_line(),
+    "box" = p + cubble::geom_glyph_box()
+  )
+
+  res <- ggplot2::layer_data(out)
+
+  if (component %in% c("line", "box")) res <- res %>% dplyr::rename(gx = x, gy = y, gid = group)
+  res
 }
 
-# Create reference boxes for a glyph plot
-ref_boxes <- function(data, fill = NULL) {
-  stopifnot(is.glyphplot(data))
-  glyph <- attributes(data)
-  cells <- data.frame(unique(data[c(glyph$x_major, glyph$y_major, "gid", fill)]))
+add_ref_lines <- function(data, color = "white", size = 1.5, ...){
 
-  df <- data.frame(xmin = cells[[glyph$x_major]] - glyph$width / 2,
-      xmax = cells[[glyph$x_major]] + glyph$width / 2,
-      ymin = cells[[glyph$y_major]] - glyph$height / 2,
-      ymax = cells[[glyph$y_major]] + glyph$height / 2)
-  if (!is.null(fill)){
-    df$fill <- cells[[fill]]
-  }
-  df
+  data <- glyph_layer(data, component = "line")
+  ggplot2::geom_path(data = data, color = color, size = size, ...)
+}
+
+add_ref_boxes <- function(data, var_fill = NULL, color = "white", size = 0.5,
+                          fill = NA, ...){
+
+  data <- glyph_layer(data, component = "box")
+  data <- data %>% dplyr::select(gid, gx, gy, xmin:ymax)
+
+  suppressWarnings(ggplot2::geom_rect(
+    data = data,
+    aes_all(names(data)),
+    color = color, size = size,fill = fill, ...))
+
 }
 
 
@@ -147,12 +134,13 @@ ref_boxes <- function(data, fill = NULL) {
 #'   string) for the major x and y axes.  Together, the
 #    combination of \code{x_major} and \code{y_major} specifies a grid cell.
 #' @export
-#' @author Di Cook, Heike Hofmann, Hadley Wickham
-glyphplot <- function(data, width, height, polar, x_major, y_major) {
-  structure(data,
-    width = width, height = height, polar = polar,
-    x_major = x_major, y_major = y_major,
-    class = c("glyphplot", "data.frame"))
+#' @author Di Cook, Heike Hofmann, Hadley Wickham, Sherry Zhang
+glyphplot <- function(data, width, height, polar, x_major, y_major, x_minor, y_minor) {
+  tibble::new_tibble(data, nrow = nrow(data),
+            width = width, height = height, polar = polar,
+            x_major = x_major, y_major = y_major,
+            x_minor = x_minor, y_minor = y_minor,
+            class = "glyphplot")
 }
 #' @export
 #' @rdname glyphplot
@@ -163,140 +151,32 @@ is.glyphplot <- function(x) {
 #' @rdname glyphplot
 "[.glyphplot" <- function(x, ...) {
   glyphplot(NextMethod(),
-    width = attr(x, "width"), height = attr(x, "height"),
-    x_major = attr(x, "x_major"), y_major = attr(x, "y_major"),
-    polar = attr(x, "polar"))
+            width = attr(x, "width"), height = attr(x, "height"),
+            x_major = attr(x, "x_major"), y_major = attr(x, "y_major"),
+            x_minor = attr(x, "x_minor"), y_minor = attr(x, "y_minor"),
+            polar = attr(x, "polar"))
 }
 
 #' @param x glyphplot to be printed
 #' @param ... ignored
 #' @export
 #' @rdname glyphplot
-#' @method print glyphplot
-print.glyphplot <- function(x, ...) {
-  NextMethod()
+#' @importFrom tibble tbl_sum
+#' @method tbl_sum glyphplot
+tbl_sum.glyphplot <- function(x, ...) {
   if (attr(x, "polar")) {
-    cat("Polar ")
+    coord <- "polar"
   } else {
-    cat("Cartesian ")
+    coord <- "cartesian"
   }
+
+  dim <- glue::glue(nrow(x), " x ", ncol(x))
   width <- format(attr(x, "width"), digits = 3)
   height <- format(attr(x, "height"), digits = 3)
 
-  cat("glyphplot: \n")
-  cat("  Size: [", width, ", ", height,  "]\n", sep = "")
-  cat("  Major axes: ", attr(x, "x_major"), ", ", attr(x, "y_major"), "\n",
-    sep = "")
-  # cat("\n")
-}
+  size <- glue::glue("[", width, ", ", height,  "]\n", sep = "")
+  axes <- glue::glue(attr(x, "x_major"), ", ", attr(x, "y_major"), "\n",
+      sep = "")
 
-
-# Relative dimensions --------------------------------------------------------
-
-# Relative dimensions
-#
-# @param x numeric value between 0 and 1
-# rel <- function(x) {
-#   structure(x, class = "rel")
-# }
-# @export
-# rel <- ggplot2::rel
-
-# @rdname rel
-# @param ... ignored
-# print.rel <- function(x, ...) {
-#   print(noquote(paste(x, " *", sep = "")))
-# }
-## works even though it is not exported
-# @export
-# ggplot2::print.rel
-
-# @rdname rel
-# is.rel <- function(x) {
-#   inherits(x, "rel")
-# }
-## only used internally.  and ggplot2 has this exported
-# @export
-# ggplot2:::is.rel
-is.rel <- ggplot2:::is.rel
-
-# Rescaling functions --------------------------------------------------------
-
-#' Rescaling functions
-#'
-#' @param x numeric vector
-#' @param xlim value used in \code{range}
-#' @name rescale01
-
-
-#' @export
-#' @rdname rescale01
-range01 <- function(x) {
-  rng <- range(x, na.rm = TRUE)
-  (x - rng[1]) / (rng[2] - rng[1])
-}
-
-#' @export
-#' @rdname rescale01
-max1 <- function(x) {
-  x / max(x, na.rm = TRUE)
-}
-#' @export
-#' @rdname rescale01
-mean0 <- function(x) {
-  x - mean(x, na.rm = TRUE)
-}
-#' @export
-#' @rdname rescale01
-min0 <- function(x) {
-  x - min(x, na.rm = TRUE)
-}
-#' @export
-#' @rdname rescale01
-rescale01 <- function(x, xlim=NULL) {
-  if (is.null(xlim)) {
-    rng <- range(x, na.rm = TRUE)
-  } else {
-    rng <- xlim
-  }
-  (x - rng[1]) / (rng[2] - rng[1])
-}
-#' @export
-#' @rdname rescale01
-rescale11 <- function(x, xlim=NULL) {
-  2 * rescale01(x, xlim) - 1
-}
-
-#' Add reference lines for each cell of the glyphmap.
-#'
-#' @param data A glyphmap structure.
-#' @param color Set the color to draw in, default is "white"
-#' @param size Set the line size, default is 1.5
-#' @param ... other arguments passed onto [ggplot2::geom_line()]
-#' @export
-add_ref_lines <- function(data, color = "white", size = 1.5, ...){
-  rl <- ref_lines(data)
-  geom_path(data = rl, color = color, size = size, ...)
-}
-
-#' Add reference boxes around each cell of the glyphmap.
-#'
-#' @param data A glyphmap structure.
-#' @param var_fill Variable name to use to set the fill color
-#' @param color Set the color to draw in, default is "white"
-#' @param size Set the line size, default is 0.5
-#' @param fill fill value used if \code{var_fill} is \code{NULL}
-#' @param ... other arguments passed onto [ggplot2::geom_rect()]
-#' @export
-add_ref_boxes <- function(data, var_fill = NULL, color = "white", size = 0.5,
-                          fill = NA, ...){
-  rb <- ref_boxes(data, var_fill)
-  if (!is.null(var_fill)){
-    geom_rect(aes_all(names(rb)), data = rb,
-              color = color, size = size, inherit.aes = FALSE, ...)
-  }
-  else{
-    geom_rect(aes_all(names(rb)), data = rb,
-              color = color, size = size, inherit.aes = FALSE, fill = fill, ...)
-  }
+  c(glyphplot = dim, coord = coord, size = size, "major axes" = axes)
 }
