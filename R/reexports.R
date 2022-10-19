1f15c988ae880cac1f1cf53ce3fe6fec52ae7a12
Joseph Larmarange <joseph@larmarange.net>
2022-10-19 12:05:46 +0200

    reexport ggcoef_model() family

---
+++ R/reexports.R
@@ -0,0 +1,19 @@
+# reexports from ggstats -------------------
+
+#' @importFrom ggstats ggcoef_model
+#' @export
+ggstats::ggcoef_model
+
+#' @importFrom ggstats ggcoef_compare
+#' @export
+ggstats::ggcoef_compare
+
+#' @importFrom ggstats ggcoef_multinom
+#' @export
+ggstats::ggcoef_multinom
+
+#' @importFrom ggstats ggcoef_plot
+#' @export
+ggstats::ggcoef_plot
+
+
