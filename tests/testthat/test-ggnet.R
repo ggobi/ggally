747dab2e7e8d57cd5fb49d0f8ec46d33d9c04551
Michael Chirico <chiricom@google.com>
2024-05-15 14:00:41 -0700

    also test-ggnet.R

--- tests/testthat/test-ggnet.R
+++ tests/testthat/test-ggnet.R
@@ -3,14 +3,14 @@ if ("package:igraph" %in% search()) {
   detach("package:igraph")
 }
 
-rq(network) # network objects
-rq(sna) # placement and centrality
+skip_if_not(rq(network)) # network objects
+skip_if_not(rq(sna)) # placement and centrality
 
-rq(ggplot2) # grammar of graphics
-rq(grid) # arrows
-rq(scales) # sizing
+skip_if_not(rq(ggplot2)) # grammar of graphics
+skip_if_not(rq(grid)) # arrows
+skip_if_not(rq(scales)) # sizing
 
-rq(intergraph) # test igraph conversion
+skip_if_not(rq(intergraph)) # test igraph conversion
 
 test_that("examples", {
   ### --- start: documented examples
