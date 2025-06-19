9780e262763ecd7e2603bfee738aec03fd0e101a
Barret Schloerke <barret@posit.co>
2025-06-19 00:56:27 -0400

    Add < R 4.3 S7 package support

--- R/GGally-package.R
+++ R/GGally-package.R
@@ -8,3 +8,7 @@
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
