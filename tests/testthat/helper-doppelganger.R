5a18093b65c8c1da031e032fed01cbce81e47e20
Barret Schloerke <barret@posit.co>
2026-02-02 14:47:42 -0500

    Standardize snapshot names in doppelganger helper
    Introduces a local string standardization function to ensure snapshot names match vdiffr's expectations. Adds checks for name standardization in interactive mode and improves handling of snapshot file naming and skipping logic for specific test environments.

--- tests/testthat/helper-doppelganger.R
+++ tests/testthat/helper-doppelganger.R
@@ -1,25 +1,46 @@
+vdiffr__str_standardise <- function(s) {
+  sep <- "-"
+  s <- gsub("[^a-z0-9]", sep, tolower(s))
+  s <- gsub(paste0(sep, sep, "+"), sep, s)
+  s <- gsub(paste0("^", sep, "|", sep, "$"), "", s)
+  s
+}
+
 ggally_expect_doppelganger <- function(name, plot) {
+  name <- vdiffr__str_standardise(name)
+
+  # When testing locally, make sure the name is standardised with what vdiffr expects
+  if (interactive()) {
+    internal_str_standardise <- getFromNamespace("str_standardise", "vdiffr")
+    if (!identical(internal_str_standardise(name), name)) {
+      stop("Snapshot name '", name, "' is not standardised")
+    }
+  }
+
   if (
     identical(
       tolower(Sys.getenv("_R_CHECK_DEPENDS_ONLY_", "false")),
       "true"
     )
   ) {
-    skip("Skipping vdiffr tests on depends-only check")
+    file <- paste0(name, ".svg")
+    testthat::announce_snapshot_file(name = file)
+
+    message("Skipping vdiffr tests on depends-only check")
+    expect_true(TRUE) # Avoid empty test
+    return()
   }
+
   if (packageVersion("ggplot2") < "3.5.2.9001") {
     # Keep snapshot around, but skip the test
-    vdiffr__str_standardise <- getFromNamespace("str_standardise", "vdiffr")
-    file <- paste0(
-      vdiffr__str_standardise(name),
-      ".svg"
-    )
+    file <- paste0(name, ".svg")
     testthat::announce_snapshot_file(name = file)
     # Go through the whole process of writing the SVG
     # to ensure that the file can be created, using all the gtable code
     vdiffr::write_svg(plot, tempfile(file, fileext = ".svg"))
     expect_true(TRUE) # Avoid empty test
-  } else {
-    vdiffr::expect_doppelganger(name, plot)
+    return()
   }
+
+  vdiffr::expect_doppelganger(name, plot)
 }
